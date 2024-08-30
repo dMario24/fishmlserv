3d7e06a04b1ca465f7fef49ed075b2a6d2364629
dMario24 <data.mario24@gmail.com>
2024-08-30 12:12:21 +0900

    simple prediction

--- src/fishmlserv/main.py
+++ src/fishmlserv/main.py
@@ -11,3 +11,39 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/fish")
+def fish(length: float, weight:float):
+    """
+    물고기의 종류 판별기
+
+    Args:
+        length (float): 물고기 길이(cm)
+        weight (float): 물고기 무게(g)
+
+    Returns:
+        dict: 물고기 종류를 담은 딕셔너리
+    """
+    if length > 30.0:
+        prediction = "도미"
+    else:
+        prediction = "빙어"
+
+    return {
+                "prediction": prediction,
+                "length": length, 
+                "weight": weight
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
