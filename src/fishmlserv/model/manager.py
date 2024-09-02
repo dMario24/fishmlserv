c0425f828c7b125010f05489d3bd740820dc6924
dMario24 <data.mario24@gmail.com>
2024-09-02 12:25:58 +0900

    train_scaled

---
+++ src/fishmlserv/model/manager.py
@@ -0,0 +1,10 @@
+
+def get_model_path():
+    # import os ...
+    # 이 함수 파일의 절대 경로를 받아온다.
+    # 절대 경로를 이용해 model.pkl 의 경로를 조합
+    # 조합된 경로를 리턴 = 끝
+
+    # 사용 fastapi main.py 에서 아래와 같이 사용
+    # from fishmlserv.model.manager import get_model_path
+    return "living for today"
