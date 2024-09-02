e13296615de106a36a6ee23d58ccf2448a11c082
dMario24 <data.mario24@gmail.com>
2024-09-02 15:40:15 +0900

    fastapi + model

--- src/fishmlserv/main.py
+++ src/fishmlserv/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
 import pickle
+from fishmlserv.model.manager import get_model_path
 
 app = FastAPI()
 
@@ -25,16 +26,16 @@ def fish(length: float, weight:float):
         dict: 물고기 종류를 담은 딕셔너리
     """
     ### 모델 불러오기
-    #with open("/home/diginori/code/fishmlserv/note/model.pkl", "rb") as f:
-    #    fish_model = pickle.load(f)
+    model_path = get_model_path()
+    with open(model_path, "rb") as f:
+        fish_model = pickle.load(f)
 
-    #prediction = fish_model.predict([[length, weight]])
+    prediction = fish_model.predict([[length, weight]])
 
-    fish_class = "몰라"
-    #if prediction[0] == 1:
-    #    fish_class = "도미"
-    #else:
-    #    fish_class = "빙어"
+    if prediction[0] == 1:
+        fish_class = "도미"
+    else:
+        fish_class = "빙어"
 
     return {
                 "prediction": fish_class,
